7329e6f91b077dd5428167fefa9c91b7e233ec2d
Kim Chung <llkchung318@gmail.com>
2025-09-25 19:46:00 +0700

    init project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+      '~hooks': path.resolve(__dirname, './src/hooks'),
+      '~assets': path.resolve(__dirname, './src/assets'),
+      '~components': path.resolve(__dirname, './src/components'),
+      '~utils': path.resolve(__dirname, './src/utils'),
+      '~services': path.resolve(__dirname, './src/services'),
+      '~helpers': path.resolve(__dirname, './src/helpers'),
+      '~screens': path.resolve(__dirname, './src/screens'),
+      '~contexts': path.resolve(__dirname, './src/contexts'),
+    },
+  },
 })
