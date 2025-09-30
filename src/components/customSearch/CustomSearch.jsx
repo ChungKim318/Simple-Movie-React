f5322f51400adc903aad875c622397801feb8552
chungllk <chungbanh318@gmail.com>
2025-09-30 14:12:13 +0700

    Develop customSearch and MoviePage

---
+++ src/components/customSearch/CustomSearch.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const CustomSearch = () => {
+  return (
+    <div className="flex mb-10">
+      <div className="flex-1">
+        <input
+          type="text"
+          className="w-full p-4 bg-slate-800 outline-none text-white"
+          placeholder="Type here to search..."
+        />
+      </div>
+      <button className="p-4 bg-primary text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+      </button>
+    </div>
+  )
+}
+
+export default React.memo(CustomSearch)
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import MovieList from '~/components/movies/MovieList'
+import useSWR from 'swr'
+import { fetcher } from '~/config'
+import MovieCard from '~/components/movies/MovieCard'
+import CustomSearch from '~/components/customSearch/CustomSearch'
 
 const MoviePage = () => {
-  return <div>MoviePage</div>
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+    fetcher
+  )
+
+  const movies = data?.results || []
+
+  return (
+    <div className="py-10 page-container">
+      <CustomSearch />
+      <div className="grid grid-cols-4 gap-10">
+        {movies.length > 0 &&
+          movies.map((item, index) => (
+            <MovieCard key={item.id + index.toString()} item={item} />
+          ))}
+      </div>
+    </div>
+  )
 }
 
 export default MoviePage
